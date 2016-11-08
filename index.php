4240aba4c9dd64b4ebd84dc24cd5dfb67f83f652
raihanyuwono <raihanyuwono@hotmail.com>
2016-11-08 16:25:37 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<title>Find Your Match</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+	</head>
+	<body style="color: #6f7682;">
+		
+		<nav id="nav">
+			<ul id="nav-wrapper">
+				<li><a href="#"><b>Home</b></a></li>
+				<li><a href="#"><b>About Us</b></a></li>
+			</ul>
+		</nav>
+
+		<div class="flex-center full-height home">
+			<div class="content">
+			
+			</div>
+		</div>
+
+	</body>
+</html>
